1e921f82d52a076d7da402ae9c34e8c07798d551
Andre Pitombeira <andrepitombeira@gmail.com>
2015-10-08 18:07:27 +0200

    Project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanetExpress::Application.config.secret_token = '78e2d8895f4b9656a0a19145e320be7f3931d72d04b92539dbd812bd0e8c8ed45625bcec5d3d6acf26b8b617d4cfec5c98a2c70c3642bb009d543f343bda5cc0'
